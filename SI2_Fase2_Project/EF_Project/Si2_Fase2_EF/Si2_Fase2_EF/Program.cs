d4c0e1055aefe8bf7e085d09c362e67331d632b2
Tiago Pereira <tiagomrp06@gmail.com>
2019-12-20 19:14:42 +0000

    Implementei o exercicio H
    Diversas alterações ao projecto

--- SI2_Fase2_Project/EF_Project/Si2_Fase2_EF/Si2_Fase2_EF/Program.cs
+++ SI2_Fase2_Project/EF_Project/Si2_Fase2_EF/Si2_Fase2_EF/Program.cs
@@ -9,30 +9,76 @@ namespace Si2_Fase2_EF
     {
         static void Main(string[] args)
         {
-            Triplos triplos = new Triplos()
+            using (var ctx = new PilimEntities())
             {
-                Identificacao = "US0253985401",
-                Dia = new DateTime(2019, 12, 20, 13, 2, 0),
+                ExercicioF("FR0004548873"); //Aceder procedimento p_actualizaValorDiario
+                //ExercicioG(ctx,"FR0004548873"); //CalcularMedia6Meses Instrumento
+                //ExercicioH("FR0004548873"); //Actualizar dados instrumento
+                Console.ReadLine();
+            }
+        }
+
+        private static void ExercicioF(String isin)
+        {
+            Registo reg;
+            var triplo = new Triplos()
+            {
+                Identificacao = isin,
+                Dia = new DateTime(2019, 12, 28, 14, 3, 0),
                 Valor = 500,
                 Observado = false
             };
             using (var ctx = new PilimEntities())
             {
-                ctx.Triplos.Add(triplos);
+                ctx.Triplos.Add(triplo);
                 ctx.SaveChanges();
-
-                /*ctx.Triplos.Attach(triplos);
-                ctx.Triplos.Remove(triplos);
-                ctx.SaveChanges();*/
-                Registo reg = ctx.Registo.Find(triplos.Identificacao, new DateTime(2019, 12, 20));
-                Console.WriteLine(reg.ToString());
+                reg = ctx.Registo.Find(triplo.Identificacao, new DateTime(2019, 12, 20));
+                if (reg != null) Console.WriteLine(reg.ToString());
                 ctx.p_actualizaValorDiario();
                 ctx.SaveChanges();
-                Registo regAfter = ctx.Registo.Find(triplos.Identificacao, new DateTime(2019, 12, 20));
-                Console.WriteLine("Procedure called");
-                Console.WriteLine(regAfter.ToString());
-                Console.ReadLine();
             }
+
+            using (var ctx = new PilimEntities())
+            {
+
+                reg = ctx.Registo.Find(triplo.Identificacao, new DateTime(2019, 12, 20));
+                Console.WriteLine("Procedure called");
+                if (reg != null) Console.WriteLine(reg.ToString());
+            }
+        }
+
+        private static void ExercicioG(PilimEntities ctx, string instrumentoIsin)
+        {
+            throw new NotImplementedException();
+        }
+        private static void ExercicioH(string isin)
+        {
+            Instrumento_Financeiro inst;
+            using (var ctx = new PilimEntities())
+            {
+                Console.WriteLine("Instrumento Antes do Procedimento:");
+                inst = ctx.Instrumento_Financeiro.Find(isin);
+                if (inst != null) Console.WriteLine("Valor Actual do Instrumento<" + isin + "> :" + inst.ValorAtual);
+                var novoTriplo = new Triplos()
+                {
+                    Identificacao = isin,
+                    Dia = new DateTime(2019, 12, 28, 8, 0, 0),
+                    Valor = 540,
+                    Observado = false
+                };
+                ctx.Triplos.Add(novoTriplo);
+                ctx.SaveChanges();
+                Console.WriteLine("Criação de triplo");
+                var rows = ctx.p_actualizaValorDiario();
+                ctx.SaveChanges();
+            }
+            using (var ctx = new PilimEntities())
+            {
+                Console.WriteLine("Instrumento Depois do Procedimento:");
+                inst = ctx.Instrumento_Financeiro.Find(isin);
+                if (inst != null) Console.WriteLine("Valor Actual do Instrumento<" + isin + "> :" + inst.ValorAtual);
+            }
+
         }
     }
 }
