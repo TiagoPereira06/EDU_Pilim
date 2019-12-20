3f619d4e3c6caf04b0a0490f34a5e0a80baabd02
Pedro Tereso <pedrotereso4@gmail.com>
2019-12-20 11:40:57 +0000

    ...

--- SI2_Fase2_Project/Exercicio1/Program.cs
+++ SI2_Fase2_Project/Exercicio1/Program.cs
@@ -2,33 +2,29 @@
 using DAL_Specific;
 using Entities;
 using System;
+using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
 using System.Transactions;
 
 namespace Exercicio1
 {
-    class Program
+    class Exercicio1
     {
         static void Main(string[] args)
         {
 
-            Triplo triplo = new Triplo()
-            {
-                //(int year, int month, int day, int hour, int minute, int second);
-                Identificacao = "IE00BXC8D038",
-                Dia = new DateTime(2019, 11, 4, 10, 30, 0),
-                Valor = 420
-            };
+            string cs = ConfigurationManager.ConnectionStrings["TL51N_11"].ConnectionString;
 
-            using (var ts = new TransactionScope())
+            using (var cn = new SqlConnection(cs))
             {
-                IMapperTriplo map = new MapperTriplo();
-                map.Create(triplo);
-                ts.Complete();
+                f(cn);
+                g();
+                //h();
+                //i();
+                //j();
+                //k();
             }
-
-            Console.ReadKey();
 
 
             //String storedProcedure = "p_actualizaValorDiario";
@@ -120,46 +116,81 @@ namespace Exercicio1
 
     }
     */
-
-
-            /*
-            Console.WriteLine("Calling function!");
-            decimal val = CallMedia6Meses(con, "IE00BXC8D038");
-            Console.WriteLine("Value returned = " + val);
-            Console.ReadKey();
-            */
-
-            //InsertTriplos(con);
-            //InsertTr
         }
 
-        private static void InsertTriplos(SqlConnection con)
+        public static void f(SqlConnection con)
         {
+            String isin = "IE00BXC8D038";
+            String storedProcedure = "p_actualizaValorDiario";
 
-        }
-
-        public static decimal CallMedia6Meses(SqlConnection con, String isin)
-    {
-        // vai varrer a conexão
-        using(con)
-        {
-            using (var command = con.CreateCommand())
+            Triplo triplo1 = new Triplo()
             {
-                command.CommandType = CommandType.StoredProcedure;
-                command.CommandText = "dbo.Media6Meses";
+                Identificacao = isin,
+                Dia = new DateTime(2019, 11, 4, 10, 30, 0),
+                Valor = 400
+            };
 
+            Triplo triplo2 = new Triplo()
+            {
+                Identificacao = isin,
+                Dia = new DateTime(2019, 11, 4, 11, 0, 0),
+                Valor = 450
+            };
 
-                command.Parameters.AddWithValue("@isin", isin);
+            using (var ts = new TransactionScope())
+            {
+                IMapperTriplo map = new MapperTriplo();
+                map.Create(triplo1);
+                map.Create(triplo2);
 
-                SqlParameter returnValue = command.Parameters.Add("@RETURN_VALUE", SqlDbType.Money);
-                returnValue.Direction = ParameterDirection.ReturnValue;
-
-                con.Open();
-                command.ExecuteScalar();    
-
-                return (decimal)returnValue.Value;
+                //CALL STORED PROCEDURE
+                using (SqlCommand cmd = new SqlCommand(storedProcedure, con) { CommandType = System.Data.CommandType.StoredProcedure })
+                {
+                    try
+                    {
+                        con.Open();
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e);
+                    }
+                    finally
+                    {
+                        con.Close();
+                    }
+                }
+                ts.Complete();
             }
+
+
         }
+
+        public static void g()
+        {
+
+        }
+
+        public static decimal CallMedia6Meses(SqlConnection con, String isin) {
+        // vai varrer a conexão
+            using(con) {
+                using (var command = con.CreateCommand())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "dbo.Media6Meses";
+
+
+                    command.Parameters.AddWithValue("@isin", isin);
+
+                    SqlParameter returnValue = command.Parameters.Add("@RETURN_VALUE", SqlDbType.Money);
+                    returnValue.Direction = ParameterDirection.ReturnValue;
+
+                    con.Open();
+                    command.ExecuteScalar();    
+
+                    return (decimal)returnValue.Value;
+                }
+            }
     }
 
     }
